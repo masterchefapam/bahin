2c661be194bfb79f8b1e8db837f3278751ce793e
apam <masterchefapam@gmail.com>
2026-01-01 16:45:26 +0800

    update jan5 -11

--- js/main.js
+++ js/main.js
@@ -11,9 +11,10 @@ const events = {
    
    
   
-     "2025-12-21": [{ title: "Click to view", image: "images/Dec15-21.jpg" }],
+
      "2025-12-28": [{ title: "Click to view", image: "images/dec22-28.jpg" }],
      "2026-01-04": [{ title: "Click to view", image: "images/dec29-Jan4.jpg" }],
+      "2026-01-06": [{ title: "Click to view", image: "images/Jan5-11.jpg" }],
 };
 
 // --- SAFARI SAFE DATE HELPERS ---
@@ -181,6 +182,8 @@ function toggleMenu() {
 window.addEventListener('DOMContentLoaded', () => {
  
  
-  generateWeeks(safeDate("2025-12-15"), 4);
+  generateWeeks(safeDate("2025-12-29"), 2);
+  scrollToCurrentWeek();
+   generateWeeks(safeDate("2026-01-01"), 2);
   scrollToCurrentWeek();
 });
