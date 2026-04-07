9e1f7126e3701a457f38c02b9f650996c4f2225a
apam <masterchefapam@gmail.com>
2026-04-07 21:59:20 +0800

    april20 update

--- js/main.js
+++ js/main.js
@@ -19,9 +19,9 @@ const events = {
         
          
        
-    
-          "2026-04-07": [{ title: "Click to view", image: "images/April6-12.jpg" }],
-           "2026-04-13": [{ title: "Click to view", image: "images/april13-19.jpg" }],
+          "2026-04-06": [{ title: "Click to view", image: "images/April6-12.jpg" }],
+            "2026-04-13": [{ title: "Click to view", image: "images/April13-19.jpg" }],
+            "2026-04-20": [{ title: "Click to view", image: "images/april-26.jpg"}]
          
 };
 
@@ -193,7 +193,8 @@ window.addEventListener('DOMContentLoaded', () => {
  
 
    
-    generateWeeks(safeDate("2026-04-01"),3);
+    generateWeeks(safeDate("2026-04-06"),4);
+   
     
   scrollToCurrentWeek();
 });
