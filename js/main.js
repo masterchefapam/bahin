f4ced2b99652c52b88cf93adf274e9cd44bd7243
apam <masterchefapam@gmail.com>
2026-08-01 15:01:02 +0800

    august3 update

--- js/main.js
+++ js/main.js
@@ -9,10 +9,10 @@ const events = {
           
                 
                
-                     "2026-07-12": [{ title: "Click to view", image: "images/RC.jpg"}],
                       "2026-07-19": [{ title: "Click to view", image: "images/July13-19.jpg"}],
                         "2026-07-26": [{ title: "Click to view", image: "images/July20-26.jpg"}],
                          "2026-08-02": [{ title: "Click to view", image: "images/July27-Aug2.jpg"}],
+                           "2026-08-09": [{ title: "Click to view", image: "images/august3-9.jpg"}],
                       
          
 };
@@ -186,7 +186,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
    
    
-     generateWeeks(safeDate("2026-07-05"),5);
+     generateWeeks(safeDate("2026-07-20"),4);
   
    
     
